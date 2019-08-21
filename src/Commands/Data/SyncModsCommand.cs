d68c41157485dbac2959c053313c5b69fb222cd0
Jay Whaley <jayw685@gmail.com>
2019-08-20 21:48:42 -0500

    -> Following template and trying to flesh things out for just the mods (not the assets yet)

---
+++ src/Commands/Data/SyncModsCommand.cs
@@ -0,0 +1,21 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSM.Commands.Data
+{
+    /// <summary>
+    /// Command to sync mods using the hosting server as the target mod state.
+    /// </summary>
+    [ProtoContract]    
+    public class SyncModsCommand : CommandBase
+    {
+        /// <summary>
+        /// List of mods currently on the server.
+        /// </summary>
+        [ProtoMember(1)]
+        public List<string> Mods { get; set; }
+    }
+}
---
+++ src/Commands/Handler/SyncModsHandler.cs
@@ -0,0 +1,25 @@
+﻿using CSM.Commands.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSM.Networking;
+
+namespace CSM.Commands.Handler
+{
+    public class SyncModsHandler : CommandHandler<SyncModsCommand>
+    {
+        public override void Handle(SyncModsCommand command)
+        {
+            var mask = SteamHelper.GetOwnedDLCMask();
+            foreach(var dlc in mask.DLCs())
+            {
+                // Checks availability on the server?
+                if(SteamHelper.IsDLCAvailable(dlc))
+                {
+                    //TODO: GET DLC
+                }
+            }
+        }
+    }
+}
